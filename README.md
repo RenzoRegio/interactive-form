94e40ad00c8ba107a66b756e037fc6892f806bfc
RenzoRegio <renzorregio@gmail.com>
2021-01-04 19:29:12 +0800

    Update

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # interactive-form
- 
+
+Additional Features for Conditional Error Messages:
+
+Field: Name
+Condition: If the name field is empty or contains any characters that returns the regex as false, then the error will show.
+Event: Keyup
+Error Message: Only use Letters, Numbers, Underscore, Hyphen
+
+Field: Email
+Condition: If the email field does not meet the regex condition, the error message will show an additional hint for properly formatting the field.
+Event: Keyup
+Error Message: (Example: user@blank.com)
+
+Field: Credit Card Number
+Condition: Credit Card Number field must contain only digits that range from 13 - 16 numbers (No other character)
+Event: Keyup
+Error Message: Numbers Only (13 - 16 digits)
+
+Field: Zip Code
+Condition: Zip Code field must contain exactly 5 numbers (No other character)
+Event: Keyup
+Error Message: Numbers Only (Exactly 5 digits)
+
+Field: CVV
+Condition: CVV field must contain exactly 3 numbers (No other character)
+Event: Keyup
+Error Message :Numbers Only (Exactly 3 digits)
--- js/script.js
+++ js/script.js
@@ -19,6 +19,7 @@ const ccNum = document.querySelector("#cc-num");
 const hints = document.querySelectorAll(".hint");
 
 let activitiesTotalCost = 0;
+let validatedFields = 0;
 
 /**
  * Default when the page loads - configures the attributes of different elements to focused, displayed, or selected.
@@ -125,32 +126,55 @@ paymentSelect.addEventListener("change", (e) => {
 });
 
 /**
- * Listens for the submit event for the whole form and validates if each of the field's requirements have been fulfilled. If not, then an error will be displayed. If it is then a green check icon will be displayed next to the label of the input.
+ * Returns true or false depending on the regex test method.
+ * @param {*} regex - The regular expression that is passed to verify if the element's value is equal to the regular expression.
+ * @param {*} element - The element that contains the input value which is then tested to check if it matches the regular expression's conditions.
+ */
+
+function regexValidation(regex, element) {
+  //Validates if the input's value fulfills the regex parameter and returns true or false.
+  return regex.test(element.value);
+}
+
+/**
+ * Adds an error effect to the elements inside the function.
+ * @param {*} element - An element passed on to the function which is then manipulated to add an error effect to the element and its correspondence.
+ * @param {*} num - The number value which goes through the hints array and selects the hint specified by the number.
+ */
+
+function invalidate(element, num) {
+  //For elements that returned false after regexValidation - provides the error layout for the specified element.
+  element.parentNode.className = "not-valid";
+  element.className = "box-validation";
+  hints[num].classList.remove("hint");
+}
+
+/**
+ * Adds a validation effect to the elements inside the function.
+ * @param {*} element - An element passed on to the function which is then manipulated to add a validation effect to the element and its correspondence.
+ */
+
+function validate(element) {
+  //For elements that returned true after regexValidation - provides the valid layout for the specified element.
+  element.parentNode.className = "valid";
+  element.className = "";
+  element.nextElementSibling.style.display = "none";
+  element.setAttribute("validated", "");
+}
+
+/**
+ * Listens for the submit event for the whole form, submits and validates if each of the field's requirements have been fulfilled.
  */
 
 form.addEventListener("submit", (e) => {
-  function regexValidation(regex, element) {
-    //Validates if the input's value fulfills the regex parameter and returns true or false.
-    return regex.test(element.value);
-  }
+  //Validates each field with the corresponding conditions.
 
-  function invalidate(element, num) {
-    //For elements that returned false after regexValidation - provides the error layout for the specified element.
-    element.parentNode.className = "not-valid";
-    element.className = "box-validation";
-    hints[num].classList.remove("hint");
-    document.documentElement.scrollTop = 0;
-  }
-  function validate(element) {
-    //For elements that returned true after regexValidation - provides the valid layout for the specified element.
-    element.parentNode.className = "valid";
-    element.className = "";
-    element.nextElementSibling.style.display = "none";
-  }
-  e.preventDefault();
   const activityHeader = activitiesContainer.firstElementChild;
   for (let i = 0; i < hints.length; i++) {
-    if (nameField.value === "") {
+    if (
+      nameField.value === "" ||
+      !regexValidation(/^[a-zA-Z0-9_-]*$/, nameField)
+    ) {
       invalidate(nameField, 0);
     } else {
       validate(nameField);
@@ -158,7 +182,7 @@ form.addEventListener("submit", (e) => {
     if (!regexValidation(/^[^@]+@\w+\.\w+/, email)) {
       invalidate(email, i);
     } else {
-      validate(email, i);
+      validate(email);
     }
     if (activitiesCost.textContent === "Total: $0") {
       activityHeader.className = "not-valid";
@@ -166,6 +190,7 @@ form.addEventListener("submit", (e) => {
     } else {
       activitiesCost.nextElementSibling.style.display = "none";
       activityHeader.className = "valid";
+      activityHeader.setAttribute("validated", "");
     }
     if (!regexValidation(/^\d{13,16}$/, ccNum)) {
       invalidate(ccNum, i);
@@ -183,6 +208,23 @@ form.addEventListener("submit", (e) => {
       validate(cvvNum);
     }
   }
+
+  //Checks whether all required fields have been validated. If it has then the form will be submitted, else the default action will be prevented.
+
+  const validatedElements = document.querySelectorAll("[validated]");
+  const creditCardContainer = document.querySelector("#credit-card");
+
+  if (
+    creditCardContainer.style.cssText === "display: none;" &&
+    validatedElements.length !== 3
+  ) {
+    e.preventDefault();
+  } else if (
+    creditCardContainer.style.cssText === "" &&
+    validatedElements.length !== 6
+  ) {
+    e.preventDefault();
+  }
 });
 
 /**
@@ -199,27 +241,20 @@ for (let i = 0; i < activitiesInput.length; i++) {
 }
 
 /**
- * Listens for the "keyup" event on all of the inputs of the creditCardBox. If the value of the input contains anything that is not a number then an error message will be shown.
+ * Listens for a "keyup" event on the nameField variable - verifies if conditions have been met and displays either a validation or error effect.
  */
 
-creditCardBox.addEventListener("keyup", () => {
-  const errorMessage = document.querySelectorAll(".js-error");
-  for (let i = 0; i < errorMessage.length; i++) {
-    if (isNaN(ccNum.value)) {
-      errorMessage[0].style.display = "block";
-    } else {
-      errorMessage[0].style.display = "none";
-    }
-    if (isNaN(zipNum.value)) {
-      errorMessage[1].style.display = "block";
-    } else {
-      errorMessage[1].style.display = "none";
-    }
-    if (isNaN(cvvNum.value)) {
-      errorMessage[2].style.display = "block";
-    } else {
-      errorMessage[2].style.display = "none";
-    }
+nameField.addEventListener("keyup", () => {
+  const nameError = document.querySelector(".js-name");
+  if (
+    nameField.value === "" ||
+    !regexValidation(/^[a-zA-Z0-9_-]*$/, nameField)
+  ) {
+    invalidate(nameField, 0);
+    nameError.style.display = "block";
+  } else {
+    validate(nameField);
+    nameError.style.display = "none";
   }
 });
 
@@ -229,33 +264,48 @@ creditCardBox.addEventListener("keyup", () => {
 
 email.addEventListener("keyup", () => {
   const emailError = document.querySelector(".js-email");
-  if (!email.value.includes("@")) {
+  if (!regexValidation(/^[^@]+@\w+\.\w+/, email)) {
     emailError.style.display = "block";
+    invalidate(email, 1, emailError);
   } else {
     emailError.style.display = "none";
+    validate(email);
   }
 });
 
 /**
- * Listens for the "keyup" event on the other job role's input value. If it includes the same value in any of the select options textContent then it will display the custom error.
+ * Listens for a three "keyup" events. Depending on the selected variable on the credit card section, it verifies if conditions have been met and displays either a validation or error effect.
  */
 
-otherJobRoleInput.addEventListener("keyup", () => {
-  const options = document.querySelectorAll("[data-js]");
-  const otherError = document.querySelector(".js-other");
-  function repeatedValue() {
-    for (let i = 0; i < options.length; i++) {
-      const val = options[i].textContent
-        .toLowerCase()
-        .includes(otherJobRoleInput.value.toLowerCase());
-      if (val && otherJobRoleInput.value !== "") {
-        return true;
-      }
+const errorMessage = document.querySelectorAll(".js-error");
+for (let i = 0; i < errorMessage.length; i++) {
+  ccNum.addEventListener("keyup", () => {
+    if (!regexValidation(/^\d{13,16}$/, ccNum)) {
+      errorMessage[0].style.display = "block";
+      invalidate(ccNum, 3);
+    } else {
+      errorMessage[0].style.display = "none";
+      validate(ccNum);
     }
-  }
-  if (repeatedValue()) {
-    otherError.style.display = "block";
-  } else {
-    otherError.style.display = "none";
-  }
-});
+  });
+
+  zipNum.addEventListener("keyup", () => {
+    if (!regexValidation(/^\d{5}$/, zipNum)) {
+      errorMessage[1].style.display = "block";
+      invalidate(zipNum, 4);
+    } else {
+      errorMessage[1].style.display = "none";
+      validate(zipNum);
+    }
+  });
+
+  cvvNum.addEventListener("keyup", () => {
+    if (!regexValidation(/^\d{3}$/, cvvNum)) {
+      errorMessage[2].style.display = "block";
+      invalidate(cvvNum, 5);
+    } else {
+      errorMessage[2].style.display = "none";
+      validate(cvvNum);
+    }
+  });
+}
